30240f092a839c3f8b2abb7184e1b49e1b6da396
Hermanhyl <hermanhyl@hotmail.com>
2024-02-09 15:15:48 +0100

    fixed parent issue

--- JS/cart.js
+++ JS/cart.js
@@ -3,8 +3,7 @@ console.log(removeCartItemButtons)
 for (var i = 0; i < removeCartItemButtons.length; i++) { 
     var button = removeCartItemButtons[i]
     button.addEventListener('click', function(event) {
-        var buttonClicked = event.target 
+        var buttonClicked = event.target
         buttonClicked.parentElement.parentElement.remove()
     })
-
 }
