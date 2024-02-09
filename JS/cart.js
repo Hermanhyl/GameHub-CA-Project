117e7d3b9d99b46ee8a0849dd8759525d373d4d3
Hermanhyl <hermanhyl@hotmail.com>
2024-02-09 15:32:18 +0100

    code reverted

--- JS/cart.js
+++ JS/cart.js
@@ -1,9 +1,8 @@
 var removeCartItemButtons = document.getElementsByClassName('btn-danger')
 console.log(removeCartItemButtons)
-for (var i = 0; i < removeCartItemButtons.length; i++) { 
+for (var i = 0; removeCartItemButtons.length; i++) {
     var button = removeCartItemButtons[i]
-    button.addEventListener('click', function(event) {
-        var buttonClicked = event.target
-        buttonClicked.parentElement.parentElement.remove()
+    button.addEventListener('click', function() {
+        console.log('clicked')
     })
 }
