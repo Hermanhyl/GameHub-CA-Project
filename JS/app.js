94eaf47e91cb2d684aabb8e027c0cfeb38ffcb30
Hermanhyl <hermanhyl@hotmail.com>
2024-02-23 13:20:18 +0100

    updated code

---
+++ JS/app.js
@@ -0,0 +1,47 @@
+// variables
+
+// const cartButton = document.querySelector('cart-btn');
+// const removeButton = document.querySelector('btn-danger');
+// const cartQuantity = document.querySelector('cart-quantity-input');
+// const addToCart = document.querySelector('btn-shop-primary');
+// const cartContent = document.querySelector('games_container');
+// const cartItems = document.querySelector('shop-item');
+// const title = document.querySelector('shop-item-title');
+// const price = document.querySelector('shop-item-price');
+// const imageSrc = document.querySelector('shop-item-img');
+// const cartRow = document.querySelector('div');
+// const cartTotal = document.querySelector('cart-total-price');
+
+// // Cart
+// let cart = []
+
+// // Getting the products
+// class Products { 
+// async getProducts(){
+//     try {
+//         let result = await fetch("products.json");
+//         return result; 
+//         } catch (error) {
+//             connsole.log(error);
+//         }
+//     }
+// }
+// // Display products
+// class UI {
+
+// }
+
+// // Local Storage
+// class Storage {
+
+// }
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const ui = new UI();
+//     const products = new Products();
+
+//     // Get all products
+//     products.getProducts().then(data => console.log(data));
+// });
+
+
---
+++ JS/products.js
@@ -0,0 +1,32 @@
+const products = [
+    {
+        id: 0,
+        title: 'Flight Simulator',
+        price: '25.99',
+        imgSrc: './Images/Moosh_Flight_simulator_game_cover_plane_fighter_jet_helicopter__2379d3cd-9573-476d-910d-b435d288e5d4.png',
+    },
+    {
+        id: 1,
+        title: 'Happy Blocks',
+        price: '29.99',
+        imgSrc: "../Images/Moosh_game_covers_platform_games_blocks_pixels_colorful_happy_b7b5248d-2556-4fb8-afe4-860412b69561.png",
+    },
+    {
+        id: 2,
+        title: 'Hell On Earth',
+        price: '35.99',
+        imgSrc: "../Images/Moosh_game_covers_shooter_games_colorful_explotians_guns_helico_29735956-aeb8-43bb-b633-6208381c675b.png",
+    },
+    {
+        id: 3,
+        title: 'Racers',
+        price: '$29.99',
+        imgSrc: "../Images/Moosh_race_car_game_cover_cover_game_sports_car_musle_car_hot_r_1c75769b-cddf-48ae-a56a-1467fec76d75.png",
+    },
+    {
+        id: 4,
+        title: 'Raider',
+        price: '$29.99',
+        imgSrc: "../Images/Moosh_viking_game_cover_viking_ship_warrior_helmets_shields_riv_fde64b89-baa8-4692-a76a-3937a28dd11b.png",
+    }
+] 
