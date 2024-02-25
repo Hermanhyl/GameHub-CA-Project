0c0ce6448c8962140d1092394b273f1fd0cd4f45
Hermanhyl <hermanhyl@hotmail.com>
2024-02-25 16:42:26 +0100

    display game

--- JS/shop.js
+++ JS/shop.js
@@ -1,24 +1,52 @@
 import { displayGames } from "../JS/home.js";
 
-const createAddEventListenerGenre = (games) => {
-    document.querySelector ('#genre-ul').forEach((Option) => {
-        Option.addEventListener('click', () => {
-            filterGamesByGenre(games, Option.textContent);
-        });
-    });
-}
+displayGames();
 
-createAddEventListenerGenre(games);
+// Function to add click event listener to each genre option
+// const createAddEventListenerGenre = (games) => {
+//     const selectElement = document.querySelector('#genres');
+//     selectElement.addEventListener('change', (event) => {
+//         const selectedGenre = event.target.value;
+//         filterGamesByGenre(games, selectedGenre);
+//     });
+// }
 
-const filterGamesByGenre = (games, ) => {
-    if (listText === 'Show All') {
-        displayGames(games);
-    }
-    else {
-        let filteredList = games.filter((games) =>
-        game.genre === listText);
+// const filterGamesByGenre = (games, selectedGenre) => {
+//     if (selectedGenre === 'show-all') {
+//         displayGames(games);
+//     } else {
+//         const filteredList = games.filter(game => game.genre === selectedGenre);
+//         displayGames(filteredList);
+//     }
+// };
 
-        displayGames(filteredList);
-    }
-    };
+// Call the function to add event listeners when the DOM content is loaded
+// document.addEventListener('DOMContentLoaded', () => {
+//     // Assuming 'games' is defined somewhere with the list of games
+//     const games = [cart]; // Replace [...] with your list of games
 
+//     // Add event listeners for genre selection
+//     createAddEventListenerGenre(games);
+// });
+
+    // import {makeJacketCard} from "../java/index.js";  
+
+    // export const createAddEventListenerGenderButtons = (jackets) => {     
+    //     document.querySelectorAll ('.gender-ul li').forEach((li)=>{         
+    //         li.addEventListener('click', ()=>{             
+    //             filterByGender(jackets, li.textContent)         
+    //         });     
+    //     }); }  
+        
+    //     const filterByGender = (jackets,listText) => {     
+    //         if (listText === 'Show All') {      
+    //             makeJacketCard(jackets);     
+    //         }     else {         
+    //             let filteredList = jackets.filter((jacket) =>         
+    //             jacket.gender === listText);          
+    //             makeJacketCard(filteredList);     
+    //         } }
+    
+    
+    // import { createAddEventListenerGenderButtons } from "../components/filter.mjs";
+    // createAddEventListenerGenderButtons(jacketList);
