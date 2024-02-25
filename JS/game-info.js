079bd3167b462d63eb6bc391346a3d68fb69e4b4
Hermanhyl <hermanhyl@hotmail.com>
2024-02-25 16:41:54 +0100

    gameDisplay

--- JS/game-info.js
+++ JS/game-info.js
@@ -42,15 +42,15 @@ generateHtmlInfoGame(game)
 //const gameHtml = generateHtmlInfoGame(game);
 //displayContainer.appendChild(gameHtml);
 
-function displayGameInfo() {
-    //const displayContainer = document.getElementById('display-game-info');
-    //const cart = JSON.parse(localStorage.getItem('game'));
+// function displayGameInfo() {
+//     const displayContainer = document.getElementById('display-game-info');
+//     const cart = JSON.parse(localStorage.getItem('game'));
 
-    // cart.forEach(function (currentGame) {
-    //     const itemHtml = generateHtmlInfoGame(currentGame);
-    //     displayContainer.appendChild(itemHtml);
-    // });
-}
+//      cart.forEach(function (currentGame) {
+//          const itemHtml = generateHtmlInfoGame(currentGame);
+//          displayContainer.appendChild(itemHtml);
+//      });
+// }
 
 function main() {
     displayGameInfo();
